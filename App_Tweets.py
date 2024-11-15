4e407162af598a975f378524d3fbf6c443ce8765
klamsal <kamallamsal@gmail.com>
2024-11-15 07:13:20 -0600

    Update App_Tweets.py

--- App_Tweets.py
+++ App_Tweets.py
@@ -32,7 +32,7 @@ random_tweet = st.sidebar.radio('Sentiment', ('positive', 'neutral', 'negative')
 st.sidebar.markdown(data.query("airline_sentiment == @random_tweet")[["text"]].sample(n=1).iat[0, 0])
 
 st.sidebar.markdown("### Number of tweets by sentiment")
-select = st.sidebar.selectbox('Visualization type', ['Bar plot', 'Pie chart'], key='1')
+select = st.sidebar.selectbox('Visualization type', ['Bar plot', 'Pie chart'], key="1_fix1")
 sentiment_count = data['airline_sentiment'].value_counts()
 sentiment_count = pd.DataFrame({'Sentiment':sentiment_count.index, 'Tweets':sentiment_count.values})
 if not st.sidebar.checkbox("Hide", True):
@@ -47,7 +47,7 @@ if not st.sidebar.checkbox("Hide", True):
 st.sidebar.subheader("When and where are users tweeting from?")
 hour = st.sidebar.slider("Hour to look at", 0, 23)
 modified_data = data[data['tweet_created'].dt.hour == hour]
-if not st.sidebar.checkbox("Close", True, key='unique_close'):
+if not st.sidebar.checkbox("Close", True, key="1_fix2"):
     st.markdown("### Tweet locations based on time of day")
     st.markdown("%i tweets between %i:00 and %i:00" % (len(modified_data), hour, (hour + 1) % 24))
     st.map(modified_data)
@@ -56,10 +56,10 @@ if not st.sidebar.checkbox("Close", True, key='unique_close'):
 
 
 st.sidebar.subheader("Total number of tweets for each airline")
-each_airline = st.sidebar.selectbox('Visualization type', ['Bar plot', 'Pie chart'], key='2')
+each_airline = st.sidebar.selectbox('Visualization type', ['Bar plot', 'Pie chart'], key="2_fix1")
 airline_sentiment_count = data.groupby('airline')['airline_sentiment'].count().sort_values(ascending=False)
 airline_sentiment_count = pd.DataFrame({'Airline':airline_sentiment_count.index, 'Tweets':airline_sentiment_count.values.flatten()})
-if not st.sidebar.checkbox("Close", True, key='2'):
+if not st.sidebar.checkbox("Close", True, key="2_fix2"):
     if each_airline == 'Bar plot':
         st.subheader("Total number of tweets for each airline")
         fig_1 = px.bar(airline_sentiment_count, x='Airline', y='Tweets', color='Tweets', height=500)
@@ -82,7 +82,7 @@ st.sidebar.subheader("Breakdown airline by sentiment")
 choice = st.sidebar.multiselect('Pick airlines', ('US Airways','United','American','Southwest','Delta','Virgin America'))
 if len(choice) > 0:
     st.subheader("Breakdown airline by sentiment")
-    breakdown_type = st.sidebar.selectbox('Visualization type', ['Pie chart', 'Bar plot', ], key='3')
+    breakdown_type = st.sidebar.selectbox('Visualization type', ['Pie chart', 'Bar plot', ], key="3_fix1")
     fig_3 = make_subplots(rows=1, cols=len(choice), subplot_titles=choice)
     if breakdown_type == 'Bar plot':
         for i in range(1):
@@ -116,7 +116,7 @@ if len(choice) > 0:
 
 st.sidebar.header("Word Cloud")
 word_sentiment = st.sidebar.radio('Display word cloud for what sentiment?', ('positive', 'neutral', 'negative'))
-if not st.sidebar.checkbox("Close", True, key='3'):
+if not st.sidebar.checkbox("Close", True, key="3_fix2"):
     st.subheader('Word cloud for %s sentiment' % (word_sentiment))
     df = data[data['airline_sentiment']==word_sentiment]
     words = ' '.join(df['text'])
